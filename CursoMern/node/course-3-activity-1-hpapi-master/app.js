5ce8795135da021026f0dda19c7a3d144c873157
fdaniel713 <fuentesdaniel342@gmail.com>
2022-02-06 14:42:10 -0300

    first commint

---
+++ CursoMern/node/course-3-activity-1-hpapi-master/app.js
@@ -0,0 +1,28 @@
+//Change the version of this program in package.json to 1.1.0
+//For all the excersices Postman or Thunder Client is recommended.
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const ApiData = require('./data.json');//should require the data.json file
+app.use(express.json());
+
+app.get('/spells/:id', (req, res) => {
+    console.log("hola");
+    //should respond with the spell with the corresponding id value from data.json    
+});
+
+app.get('/characters', (req, res) => {
+    //Should use query params to filter the hogwartsHouse and hogwartsStudent
+});
+
+app.post('/spells', (req, res) => {
+    //Should recive spell data from request body.
+    //Should validate that the properities "id", "spell" and "use" are present in the body
+    //Response should be {"operation": "add spell", "status": "accepted"} with status 200 if all the valid properities are present
+    //Response should be {"operation": "add spell", "status": "refused"} with status 400 if there is any properitie missing.
+});
+
+app.listen(port, () => {
+    console.log(`Express server started at port ${port}`) 
+    
+});
