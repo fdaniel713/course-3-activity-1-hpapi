372d7d6ca7a864c2d068af075ba68034b53f1a27
fdaniel713 <fuentesdaniel342@gmail.com>
2022-02-06 15:04:55 -0300

    activities 3

--- CursoMern/node/course-3-activity-1-hpapi-master/app.js
+++ CursoMern/node/course-3-activity-1-hpapi-master/app.js
@@ -6,23 +6,51 @@ const port = process.env.PORT || 3000;
 const ApiData = require('./data.json');//should require the data.json file
 app.use(express.json());
 
-app.get('/spells/:id', (req, res) => {
-    console.log("hola");
-    //should respond with the spell with the corresponding id value from data.json    
-});
-
-app.get('/characters', (req, res) => {
-    //Should use query params to filter the hogwartsHouse and hogwartsStudent
-});
-
-app.post('/spells', (req, res) => {
-    //Should recive spell data from request body.
-    //Should validate that the properities "id", "spell" and "use" are present in the body
-    //Response should be {"operation": "add spell", "status": "accepted"} with status 200 if all the valid properities are present
-    //Response should be {"operation": "add spell", "status": "refused"} with status 400 if there is any properitie missing.
-});
-
-app.listen(port, () => {
-    console.log(`Express server started at port ${port}`) 
-    
-});
+app.get('/data', (req, res) =>{
+    res.json (ApiData);
+   
+   });
+   
+   app.get('/data/:id', (req, res) => {
+       //should respond with the spell with the corresponding id value from data.json   
+       const id = Number(req.params.id);
+      
+         const data = ApiData.spells.find((data) => data.id === id);
+         res.json(data);
+        
+   });
+      
+   
+   app.get('/characters', (req, res) => {
+       //Should use query params to filter the hogwartsHouse and hogwartsStudent
+       const {hogwartsHouse, hogwartsStudent} = req.query;
+       
+       if (hogwartsHouse, hogwartsStudent) {
+        const response = ApiData.characters.filter(
+           (student) => student.hogwartsStudent && hogwartsHouse === student.hogwartsHouse  );
+           res.status(200).json(response);
+   
+       } else {
+         res.status(404).json({  characters: "Not Found" });
+       }
+        
+     });
+     
+   
+   
+   app.post('/Spells', (req, res) => {
+       //Should recive spell data from request body.
+       //Should validate that the properities "id", "spell" and "use" are present in the body
+       //Response should be {"operation": "add spell", "status": "accepted"} with status 200 if all the valid properities are present
+       //Response should be {"operation": "add spell", "status": "refused"} with status 400 if there is any properitie missing.
+       const { id, spell, use } = req.body;
+       if (!id || !spell || !use) {
+         res.status(400).json({ operation: "add spell", status: "refused" });
+       } else {
+         res.status(200).json({ operation: "add spell", status: "accepted" });
+       }
+   });
+   
+   app.listen(port, () => {
+       console.log(`Express server started at port ${port}`)
+   });
